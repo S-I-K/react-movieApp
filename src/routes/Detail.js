9d28e8d7b72402cf48118684a418bbf992bae702
SIK <daesik99@naver.com>
2022-10-19 13:22:13 +0900

    details page ing....

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,30 +4,39 @@ import { useParams } from 'react-router-dom';
 import styles from './Detail.module.css';
 
 export default function Detail(){
-    const { id } = useParams(); // { id } = useParams().id
-    const [movies, setMovies] = useState([]);
+    const { id } = useParams(); //{ id } = useParams().id
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
 
     async function getMovies(){
-        const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-        setMovies(json.data.movie);
-    }
-    console.log(movies);
-    
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setLoading(false);
+        setMovie(json.data.movie);
+    };
     useEffect(()=>{
         getMovies();
-    }, [])
-    
+    } ,[]);
+
+    console.log(movie);
+
 
 
     return(
         <div className={styles.wrap}>
+            {loading ? <div className={styles.loading}>Loading ...</div> : 
             <div className={styles.container}>
-                <div className={styles.content}>
-                    <img src={movies.medium_cover_image}/>
-                    <h1>{movies.title}</h1>
-                    <p>{movies.year}</p>
-                </div>
+                <img src={movie.medium_cover_image}></img>
+                <h1>{ movie.title }</h1>
+                <ul>
+                    genres :
+                    {
+                        movie.genres.map((g)=>(<li key={g}> {g} </li>))
+                    }
+                </ul>
             </div>
+            }
         </div>
     );
 }
